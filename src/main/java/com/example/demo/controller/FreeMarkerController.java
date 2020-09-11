f1e0a2edfef78137e98741c5a2c298ba418b2b50
Gt841 <841805800@qq.com>
2020-09-11 09:27:21 +0800

    0.0.1

---
+++ src/main/java/com/example/demo/controller/FreeMarkerController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/freemarker")
+public class FreeMarkerController {
+    @RequestMapping("/index")
+    public String fmIndex(Map<String,Object> map){
+        map.put("name","GT");
+        map.put("age","123");
+        return "fmIndex";
+    }
+    @RequestMapping("/index2")
+    public String fmIndex2(Map<String,Object> result){
+        result.put("name", "GT");
+        result.put("sex", "0");
+
+        List<String> listResult = new ArrayList<>();
+        listResult.add("zhangsan");
+        listResult.add("lisi");
+        listResult.add("wangwu");
+        result.put("userlist", listResult);
+        return "fmIndex2";
+    }
+}
